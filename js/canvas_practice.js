3a32a2a4f943ee2529030b6c11e6bb6b3a185921
danielehmig <ehmig15@up.edu>
2015-02-20 09:42:56 -0800

    Removed temp borders. Updated graph labels, axises, and colors.

--- js/canvas_practice.js
+++ js/canvas_practice.js
@@ -1,60 +1,38 @@
 /* 
- * Javascript file for the practice with the Canvas element in HTML5.
+ * Javascript file for the view/controller, mostly for manipulating
+ * the canvas element (graph) and the play/pause button.
  * A namespace is used to avoid polluting the global namespace.
  */
 
-var practice = practice || {};
-practice.init = {};
+var maverick = maverick || {};
+maverick.init = {};
 
-// Implement this module for the practice namespace
-practice.init = function() {
+// Implement this module for the maverick namespace
+maverick.init = function() {
 	
 	function draw() {
 		// Getting the context for the Canvas on the screen
-		var canvas = document.getElementById("tutorial");
+		var canvas = document.getElementById("graph_canvas");
 		var context = canvas.getContext("2d");
-		
-		/*
-		// Draw a 45x40 red rectangle at (10,10)
-		context.fillStyle = "rgb(200, 0, 0)";
-		context.fillRect(10, 10, 55, 50);
-		
-		// Draw a 45x40 transparent blue rectangle at (30, 30)
-		context.fillStyle = "rgba(0, 0, 200, 0.5)";
-		context.fillRect(30, 30, 55, 50);
-		*/
 		
 		// Open a new path
 		context.beginPath();
 		
 		// Set the first point (a path starts with no points)
-		context.moveTo(75, 50);
+		context.moveTo(40, 20);
 		
-		// Create from the first point to the specified point
-		context.lineTo(80, 55);
+		// Create line from the first point to the specified point
+		context.lineTo(40, 344);
 		
-		// Create a line from the second point to the last point
-		// to create the full triangle shape
-		context.lineTo(80, 45);
+		context.lineTo(490, 344);
 		
-		// Set the fill color to a partially transparent Android blue
-		context.fillStyle = "rgba(51, 181, 229, 0.5)";
+		context.lineTo(490, 339);
 		
-		// Close and fill the triangle with the fill style specified above
-		context.fill();
+		// Set the fill color to black
+		context.fillStyle = "rgb(0,0,0)";
 		
-		// Create the arc that will connect to the arrow
-		// (and create the entire refresh symbol)
-		context.beginPath();
-	
-		context.arc(85, 57, 10, -(Math.PI / 1.5), -Math.PI);
-		
-		//context.lineWidth(5);
+		// Draw the y-axis
 		context.stroke();
-		
-		
-		
-		
 		
 	}
 	
@@ -63,4 +41,4 @@ practice.init = function() {
 };
 
 // Invoke the module
-practice.init();
+maverick.init();
